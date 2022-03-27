ea097e7bfeff7926b891301427fc3206d18242e9
MaiCodes-exe <maise.b.costa@gmail.com>
2022-03-27 20:50:19 +0000

    added people and planets cards

--- src/js/component/people.js
+++ src/js/component/people.js
@@ -3,13 +3,14 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 
 
-
-export default function People() {
+export default function People({ data}) {
     return (
-        <Card style={{ width: "18rem" }}>
+        <>
+            <h1>People</h1>
+        <Card style={{ width: "18rem" }} >
           <Card.Img variant="top" src="holder.js/100px180" />
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
+            <Card.Title> </Card.Title>
             <Card.Text>
               Some quick example text to build on the card title and make up the
               bulk of the card's content.
@@ -17,6 +18,7 @@ export default function People() {
             <Button variant="primary">Go somewhere</Button>
           </Card.Body>
         </Card>
+        </>
     );
     
 }
