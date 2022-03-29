c0196b0798f0a4dae4a087f06b9da56c50d5ea89
MaiCodes-exe <maise.b.costa@gmail.com>
2022-03-29 20:17:50 +0000

    added the proptypes

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,16 @@
 import React, {useState, useEffect} from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "../../styles/home.css";
+import PropTypes from "prop-types";
 import { Navbar } from "../component/navbar";
 import People from "../component/people";
 import Planets from "../component/planets";
 
-export const Home = () => {
+export const Home = (props) => {
 	const [people, setPeople] = useState([]);
 	const [planets, setPlanets] = useState([]);
 	// const [loading, setLoading] = useState(true);
-	async function fetchPeople() {
+	async function fetchPeople(props) {
 		let res = await fetch("https://www.swapi.tech/api/people");
 		let data = await res.json();
 		setPeople(data.results)
@@ -24,20 +25,17 @@ export const Home = () => {
 		fetchPeople();
 		fetchPlanets();
 	}, []);
+	people.map
+	console.log(people)
 	return (
-    <>
-      {/* <Router>
-        <div>
-          <Switch>
-            <Route exact path="/people">
-              <People />
-            </Route>
-            <Route exact path="/planets">
-              <Planets />
-            </Route>
-          </Switch>
-        </div>
-      </Router> */}
-    </>
-  );
+		<>
+		</>
+	);
+};
+Home.propTypes = {
+  iud: PropTypes.number,
+  name: PropTypes.string,
+  height: PropTypes.string,
+  homeworld: PropTypes.string,
+  description: PropTypes.string,
 };
